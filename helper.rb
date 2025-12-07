dfb82c8beef11f2bb1536222d61cdf79f16a6266
Haru <harukwork@gmail.com>
2025-12-07 11:24:39 -0500

    PBE 12.5

--- helper.rb
+++ helper.rb
@@ -1,12 +1,136 @@
-str = "Aim for the Head
-Energetic
-Hive Mind
-Hybrid
-Multitool
-Panic Room
-Quest: Rite of the Forge God
-Righteous Fury
-Unshackled"
+str = "Akshan
+Aphelios
+Ashe
+Caitlyn
+Cassiopeia
+Corki
+Draven
+Elise
+Gangplank
+Garen
+Graves
+Jhin
+Jinx
+Kindred
+Lucian
+Miss Fortune
+Nilah
+Quinn
+Rengar
+Samira
+Senna
+Shaco
+Sivir
+Smolder
+Tristana
+Tryndamere
+Twitch
+Viego
+Xayah
+Yasuo
+Yone
+Yunara
+Zeri"
 
-arr = str.split("\n").map { |s| "**#{s}**\n\n\n" }
-puts arr
+arr = ["PAuto2 Crit Damage: 52.5% (+12% IE) -> 200% (+30% IE)
+Q Base Damage: 5/25/45/65/85 (+80% tAD) -> 45/75/105/135/165 (+70% bAD)
+E Damage: 15/30/45/60/75 (+15% tAD) -> 8/16/24/32/40 (+25% tAD)
+E Crit Damage: 157.5% (+36% IE) -> 100% (+30% IE)
+R Crit Amp: 0-50% (+20% IE) -> 0-30% (+0-9% IE)",
+"Calibrum Mark Damage: 15 (+20% bAD) -> 15 (+10% bAD)
+Sentry AD Ratio: 42/45/48/51/54/57/60% bAD -> 30/33/36/39/42/45/48% bAD
+Severum Damage: 10-40 (22-40% bAD) -> 10 (+19/22.5/26/29.5/33/36.5/40% tAD)
+Infernum Damage: 25-65 (+56-80% bAD) -> 20/35/50/65/80/95/110 (+10/11/12/13/14/15/16% bAD)
+R Crit Damage: 120% (+40% IE) -> 100-130% (+0-9% IE)",
+"AS per Level: 3.33% -> 3%
+AD per Level: 3.45 -> 3
+P Crit Amp: 75% (+40% IE) -> 100% (+30% IE)
+W AD Ratio: 110% -> 100%",
+"P Crit Amp: 0-85% (+0-34% IE) -> 0-100% (+0-30% IE)
+R Crit Amp: 0-50% -> 0-30% (+0-9% IE)",
+"Mid Quest Reward: 1-18 bonus MS -> 3-54 AP
+- Bot Quest not implemented",
+"W Base Damage per tick: 40/55/70/85/100 -> 30/45/60/75/90",
+"AD per Level: 3.6 -> 3",
+"Human Q Monster Cap: 75/100/125/150/175 -> 65/85/105/125/145 (+90% AP)
+Spider Q Monster Cap: 75/100/125/150/175 -> 65/85/105/125/145 (+90% AP) ",
+"Q Crit Damage: 175% (+40% IE) -> 200% (+30% IE)",
+"E Crit Damage: 140% (+32% IE) ->  130% (+9% IE)",
+"P Single Bullet Crit Damage: 33.75% (+18% IE) -> 35% (+10.5% IE)
+- Total Damage: 178.312% (+23.997% IE) -> 179.979% (+13.998% IE)",
+"AA Crit Damage: 150.5% (+34.4% IE) -> 150% (+22.5% IE)",
+"AS per Level: 1.4% -> 1%
+R Damage: 250/400/550 (+130% bAD) -> 200/350/500 (+120% bAD)",
+"E Base damage no longer critically strikes.
+E Mark Damage no longer critically strikes.
+E Crit Amp: 0% -> 0-50% (+0-15% IE) ",
+"AS per Level: 3.3% -> 2.5%
+AD per Level: 2.9 -> 2.5
+Q Damage: 85/115/145/175/205 (+60/75/90/105/120% bAD) -> 80/115/150/185/220 (+100% bAD)
+R Bullet Count: 22 (+1 per 4% crit chance) -> 22 (+1 per 4.5% crit chance)",
+"Q2 can now critically strike normally, and will always crit when killing the target of Q1
+Q2 Crit Damage: 175% (+40% IE) -> 150% (+15% IE)
+R Damage: 0 (+80% tAD) (+25% AP) -> 20/30/40 (+60% bAD) (+25% AP)
+R Crit Damage: 120% (+8% IE) -> 130% (+9% IE)",
+"AS per Level: 2.25% -> 2%
+Q Damage: 5/10/15/20/25 (+90/95/100/105/110% tAD) -> 10/20/30/40/50 (+100% tAD)
+Q Passive Armor Pen: 0-33% -> 0-30%
+Q Crit Amp: 0-100% -> 0-80% (+0-24% IE)
+R Mana Cost: 80 -> 100",
+"P Mark Damage: 10-95 (+16-50% tAD) -> 15-120 (+40% bAD)",
+"Q AD Ratio: 100/103.75/107.5/111.25/115% -> 100%
+Q Emp AD Ratio: 130% -> 120%",
+"Q AD Ratio: 95/102.5/110/117.5/125% -> 100%
+Q Crit Damage: 125% (+40% IE) -> 150% (+15% IE)
+R Damage: 5/15/25 (+45% AD) -> 20/40/60 (+25% AD)",
+"Total Crit Damage: 175% (+40% IE) -> 180% (+30% IE)",
+"P Backstab AD Ratio: 25% -> 15%
+Q Backstab Non Crit Damage: 155% (+40% IE) -> 150% (+15% IE)",
+"Q AD Ratio: 85% -> 70%
+Q Crit Amp: 0-50% -> 0-40% (+0-12% IE)",
+"PQ Crit Amp: 0-30% (+0-16% IE) stacks -> 0-20% (+0-6% IE) stacks
+Q Crit Amp: 0-75% (+0-40% IE) -> 0-50% (+0-16% IE)",
+"E Damage: 60/70/80/90/100 (+100/110/120/130/140% bAD) -> 60/85/110/135/160 (+80% bAD)
+E Crit Amp: 0-75% (+0-40% IE) -> 0-40% (+0-12% IE)",
+"Q Passive: 5/10/15/20/25 AD + 0.15/0.25/0.35/0.45/0.55 AD per 1% missing health -> 0 to 20/35/50/65/80 AD based on missing health (max value at 10% remaining health)
+W Slow: 30/37.5/45/52.5/60% -> 30/35/40/45/50%
+W Slow Duration: 4s -> 2.5s
+E Damage: 75/105/135/165/195 (+130% bAD) -> 70/105/140/175/210 (+100% bAD)",
+"AS per Level: 3.38% -> 3%
+AD per Level: 3.1 -> 3
+Q AS: 45/50/55/60/65% -> 40/45/50/55/60%",
+"QP Crit Damage Amp: 75% (+40% IE) -> 50% (+15% IE)
+Q Crit Amp: 0-75% -> 0-50% (+0-15% IE)
+R Crit Amp: 0-100% -> 0-70% (+0-21% IE)",
+"E Damage: 55/65/75/85/95 (+60% bAD) -> 50/65/80/95/110 (+40% bAD)
+E Crit Amp: 0-75% -> 0-50% (+0-15% IE)",
+"Total Crit Damage: 175% (+40% IE) -> 180% (+30% IE)",
+"Total Crit Damage: 175% (+40% IE) -> 180% (+30% IE)",
+"Q AS: 25/35/45/55/65% -> 20/30/40/50/60%
+RW Damage: 175/350/525 (+150% bAD) -> 160/320/480 (+120% bAD)",
+"W AD Ratio: 130% -> 120%
+W Crit Damage: 175% (+40% IE) -> 175% (+20.5% IE)
+E Damage: 20/22/24/26/28 (+12% bAD) -> 17/19/21/23/25 (+10% bAD)
+E Crit Amp: 0-85% -> 0-100% (+0-30% IE)
+R Base Damage: 200/300/400 -> 150/250/350"]
+
+h = {}
+names = str.split("\n").map { |s| "**#{s}**\n\n" }
+
+
+changes = arr.map { |c| 
+    c.split("\n").map { |s|
+        if s.start_with?("- ")
+            ret = "  * #{s[2..]}"
+        else
+            ret = "* #{s}\n"
+        end
+        ret
+    }
+}
+
+for i in 0...names.length
+    puts names[i]
+    changes[i]&.each { |s| puts s }
+    puts "\n"
+end
