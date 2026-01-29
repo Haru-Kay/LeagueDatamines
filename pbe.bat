25634a8b0547463a680e87c95e975ead157d7527
Haru <harukwork@gmail.com>
2026-01-29 14:57:37 -0500

    PBE 1.28.26

--- pbe.bat
+++ pbe.bat
@@ -9,7 +9,7 @@ start /b /wait snip-snip https://raw.communitydragon.org/pbe/game/en_us/data/men
 ::start /b /wait snip-snip https://raw.communitydragon.org/pbe/game/ --filter "items.cdtb.bin.json" -o "items" --max-depth 1
 
 ::start /b /wait snip-snip https://raw.communitydragon.org/pbe/game/maps/modespecificdata/ --filter "augments.bin.json" -o "mayhem" --max-depth 1
-start /b /wait snip-snip https://raw.communitydragon.org/pbe/plugins/rcp-be-lol-game-data/global/default/v1/ -o "game-data" --max-depth 1
+::start /b /wait snip-snip https://raw.communitydragon.org/pbe/plugins/rcp-be-lol-game-data/global/default/v1/ -o "game-data" --max-depth 1
 
 start /b /wait ruby bincompile.rb 1
 start /b /wait ritobin -o info -i bin -r -d "Data/hashes/lol" "bins" "temp"
