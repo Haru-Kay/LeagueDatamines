bd74f0130da5a0beb110dc27a7157e07cac2c804
Haru <harukwork@gmail.com>
2026-04-22 13:58:09 -0400

    PBE 4.21

--- champ.rb
+++ champ.rb
@@ -1,19 +1,13 @@
 require 'json'
 
-$champLang = []
-File.open("game-data/champion-summary.json", 'rb') { |f| 
-    c = JSON.parse(f.read()) 
-    c.each { |champ|
-        next if !champ.is_a?(Hash)
-        next if champ["alias"]&.include?("_")
-        next if champ["alias"] == "None"
-        id = champ["id"]
-        name = champ["name"].gsub("'", "").gsub("& Willump", "")
-        $champLang.push(name)
+champs = []
+
+Dir.each_child("champions") {|ch|
+    path = "champions/#{ch}/BaseStats.json"
+    File.open(path, 'rb') { |f| 
+        JSON.parse(f.read).each_value { |root| champs.push(root.dig("name")) }
+        
     }
 }
 
-puts "champs: " + $champLang.sort.join(",")
-
-puts "\n"
-
+puts champs.uniq.join(", ")
